ece2e65300df5ab1b002fe84164cf69a0433f6c4
Vincent Noel <vincent.noel@gmail.com>
2011-09-11 14:07:42 +0200

    speed up code by removing some masking

--- util.py
+++ util.py
@@ -2,14 +2,9 @@ import numpy as np
 
 def signal_ratio(denum, num, ratio_min=0, ratio_max=10, invalid=-998):
 
-    num2 = np.ma.masked_invalid(num)
-    denum2 = np.ma.masked_invalid(denum)
+    d = num / denum
 
-    d = num2 / denum2
-    idx = (denum < invalid) | (num < invalid)
-    d[idx] = np.nan
-
-    idx = (d < ratio_min) | (d > ratio_max)
+    idx = (denum < invalid) | (num < invalid) | (d < ratio_min) | (d > ratio_max)
     d[idx] = np.nan
 
     return d
