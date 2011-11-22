fc2dffa270fc65d8578b54aff4baa44499e3d4a0
Vincent Noel <vincent.noel@gmail.com>
2011-11-22 17:13:22 +0100

    Add config.py

---
+++ config.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# encoding: utf-8
+"""
+config.py
+
+Created by Vincent Noel - LMD/CNRS on 2011-11-22.
+"""
+
+major_version = 0
+minor_version = 6
+
+
+def main():
+    pass
+
+
+if __name__ == '__main__':
+    main()
+
